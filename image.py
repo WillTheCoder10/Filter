7bb9e2d16ba675d97e4e1a5b03fc7be54fed455d
WillTheCoder10 <willymesser@gmail.com>
2024-02-09 13:12:18 -0500

    Testy

--- image.py
+++ image.py
@@ -5,6 +5,7 @@
 #                                    #
 ######################################
 
+# Wee
 
 # importing PIL.Image library and os library
 from PIL import Image #from PIL import Image 
